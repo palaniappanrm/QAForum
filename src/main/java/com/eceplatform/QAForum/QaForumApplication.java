973875a3052626c80052c09860c36fb112614fe8
embeddedforum <68193664+embeddedforum@users.noreply.github.com>
2020-08-02 14:11:44 +0530

    Revert "User service related"

--- src/main/java/com/eceplatform/QAForum/QaForumApplication.java
+++ src/main/java/com/eceplatform/QAForum/QaForumApplication.java
@@ -2,23 +2,12 @@ package com.eceplatform.QAForum;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EnableJpaRepositories
 public class QaForumApplication {
-	
+
 	public static void main(String[] args) {
 		SpringApplication.run(QaForumApplication.class, args);
 	}
-	
-	/*
-	 * @RequestMapping(value = "/", method = RequestMethod.GET) public String
-	 * redirect() {
-	 * System.out.println("REDIRECTING ------------------------------------->");
-	 * return "redirect:http://localhost:8081/"; }
-	 */
-	
-	
 
 }
